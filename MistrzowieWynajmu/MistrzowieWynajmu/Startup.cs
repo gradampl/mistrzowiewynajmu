27dcf13ea8e1809bfbec534dbe4760aca69fdc45
gradampl <gr_adam@o2.pl>
2018-08-13 22:20:57 +0200

    Business logic

--- MistrzowieWynajmu/MistrzowieWynajmu/Startup.cs
+++ MistrzowieWynajmu/MistrzowieWynajmu/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MistrzowieWynajmu.Models.Database;
+using MistrzowieWynajmu.Models.Interfaces;
+using MistrzowieWynajmu.Models.Repositories;
 
 namespace MistrzowieWynajmu
 {
@@ -29,6 +31,8 @@ namespace MistrzowieWynajmu
             var dbConnectionString = @"Server=(localdb)\mssqllocaldb;Database=MistrzowieDb;Trusted_Connection=True;";
 
             services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(dbConnectionString));
+
+            services.AddScoped<IPropertyRepository, PropertyRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
